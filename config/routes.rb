24ce22e0c16b6dfb5529b76cda9aa4c256006c21
Trond Valen <trv@steria.no>
2013-07-31 11:53:32 +0200

    Completed the Getting started guide

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Railsapp::Application.routes.draw do
   get "home/index"
 
   root :to => "home#index"
-  
+
+  resources :posts do
+    resources :comments
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,10 +55,6 @@ Railsapp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
